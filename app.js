cb97b41c58ce3ad54f075c31b4e63842268e6fe0
Ömer ULUTAŞ <omerulutas@outlook.com>
2023-08-23 21:03:04 +0300

    pages changed

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController')
 
 const app = express();
 const port = 3000;
 
-//Middlewares
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+//connect db
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MİDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+//ROUTES
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 app.listen(port, () => {
   console.log(`Sunucu olarak ${port} potunda açık...`);
